722ee0bbec33242501cca1d4b706b2b64c60b65f
Harshvardhan-91 <agarwalharshvardhan081@gmail.com>
2024-07-12 12:46:56 +0530

    added footer

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Home.css";
+import Header from "../../components/Header/Header";
+import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
+import FoodDisplay from "../../components/FoodDisplay/FoodDisplay";
+
 const Home = () => {
-  return <div></div>;
+  const [category, setCategory] = useState("All");
+  return (
+    <div>
+      <Header />
+      <ExploreMenu category={category} setCategory={setCategory} />
+      <FoodDisplay category={category} />
+    </div>
+  );
 };
 
 export default Home;
