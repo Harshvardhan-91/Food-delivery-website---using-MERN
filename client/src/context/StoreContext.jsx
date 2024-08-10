04dbeeccf23ef87e71376840aa24a980d1c007f0
Harshvardhan Agarwal <154717690+Harshvardhan-91@users.noreply.github.com>
2024-08-10 21:09:48 +0530

    Update StoreContext.jsx

--- client/src/context/StoreContext.jsx
+++ client/src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "https://food-del-backend-yqk3.onrender.com";
+  const url = "https://food-del-backend-g3bq.onrender.com";
   const [token, setToken] = useState("");
   const [food_list, setFoofList] = useState([]);
   const buttonRef = useRef(null);
