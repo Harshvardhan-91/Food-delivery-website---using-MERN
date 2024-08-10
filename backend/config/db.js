4b5ff653ae3762c73fa671d75e0ab133be46905a
Harshvardhan-91 <agarwalharshvardhan081@gmail.com>
2024-08-10 20:58:24 +0530

    added

--- backend/config/db.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-
-export const ConnectDB = async()=> {
-    await mongoose.connect('mongodb+srv://agarwalharshvardhan081:**,,harsh2580**,,@cluster0.mrtzkg6.mongodb.net/food-del').then(()=> console.log('DB Connected'))
-
-}
